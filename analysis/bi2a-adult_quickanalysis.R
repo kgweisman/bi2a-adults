7619850f54a397cc880048829561b08994c03e38
kgweisman <kweisman@stanford.edu>
2015-03-03 11:52:23 -0800

    update quick analysis file, comparing us to india

--- analysis/bi2a-adult_quickanalysis.R
+++ analysis/bi2a-adult_quickanalysis.R
@@ -1,41 +1,90 @@
-n = 39
+# count observations
+n_india = d_tidy %>%
+  filter(country == "india") %>%
+  count(worker_id) %>%
+  nrow()
+
+n_us = d_tidy %>%
+  filter(country == "us") %>%
+  count(worker_id) %>%
+  nrow()
 
 # summarize by condition
 condition_summary = d_tidy %>%
-  group_by(condition) %>%
-#   group_by(swatch) %>%
+  group_by(country, condition) %>%
   summarise(mean = mean(responseCoded, na.rm = T),
             sd = sd(responseCoded, na.rm = T),
             n = length(responseCoded)/48)
 View(condition_summary)
 
-# summarize by condition and swatch
+# plot
+condsum_plot = condition_summary %>%
+  ggplot(aes(x = condition, y = mean, fill = condition)) +
+  facet_wrap(~ country) +
+  geom_bar(stat = "identity", position = "identity", width = 0.5) +
+  geom_errorbar(aes(ymin = mean - sd,
+                    ymax = mean  + sd,
+                    width = 0.1)) +
+  theme_bw() +
+  scale_fill_brewer(palette = "Set2") +
+  theme(text = element_text(size = 20),
+        legend.position = "none") +
+  labs(title = "Mean rating by condition and country\nError bars: standard deviation\n")
+print(condsum_plot)
+
+# summarize by swatch and condition
 swatch_summary = d_tidy %>%
-  group_by(swatch, condition) %>%
+  group_by(country, swatch, condition) %>%
   summarise(mean = mean(responseCoded, na.rm = T),
             sd = sd(responseCoded, na.rm = T),
             n = length(responseCoded)/48)
 View(swatch_summary)
 
-# make wide version
-swatch_summary_wide = swatch_summary %>%
-  select(-sd, -n) %>%
-  spread(condition, mean) %>%
-  arrange(animal)
-View(swatch_summary_wide)
+# plots
+# # ... india
+# swatchsum_plot_ind = swatch_summary %>%
+#   filter(country == "india") %>%
+#   ggplot(aes(x = condition, y = mean, fill = condition)) +
+#   facet_wrap( ~ swatch, ncol = 6) +
+#   geom_bar(stat = "identity", position = "identity", width = 0.5) +
+#   geom_errorbar(aes(ymin = mean - sd/sqrt(n),
+#                     ymax = mean  + sd/sqrt(n),
+#                     width = 0.1)) +
+#   theme_bw() +
+#   scale_fill_brewer(palette = "Set2") +
+#   theme(text = element_text(size = 14),
+#         axis.text.x = element_text(size = 0)) +
+#   labs(title = "Mean rating by swatch and condition: INDIA \n")
+# print(swatchsum_plot_ind)
+# 
+# # ... us
+# swatchsum_plot_us = swatch_summary %>%
+#   filter(country == "us") %>%
+#   ggplot(aes(x = condition, y = mean, fill = condition)) +
+#   facet_wrap( ~ swatch, ncol = 6) +
+#   geom_bar(stat = "identity", position = "identity", width = 0.5) +
+#   geom_errorbar(aes(ymin = mean - sd/sqrt(n),
+#                     ymax = mean  + sd/sqrt(n),
+#                     width = 0.1)) +
+#   theme_bw() +
+#   scale_fill_brewer(palette = "Set2") +
+#   theme(text = element_text(size = 14),
+#         axis.text.x = element_text(size = 0)) +
+#   labs(title = "Mean rating by swatch and condition: US \n")
+# print(swatchsum_plot_us)
 
 # plot histos by picture (faceted by condition)
 for(swatch in levels(d_tidy$swatch)) {
   this_pic = swatch
   g = d_tidy %>%
     filter(swatch == this_pic) %>% 
-    ggplot(aes(x = responseCoded, fill = swatch)) +
-      facet_wrap(~ condition) +
+    ggplot(aes(x = responseCoded, fill = country)) +
+      facet_grid(condition ~ country) +
       geom_histogram(binwidth = 0.5) +
-      coord_cartesian(xlim = c(-3:4),
-                      ylim = c(0, 10)) +
+#       coord_cartesian(xlim = c(-3:4),
+#                       ylim = c(0, 10)) +
       theme_bw() +
-      scale_fill_brewer(palette = "Set1") +
+      scale_fill_brewer(palette = "Set2") +
       theme(text = element_text(size = 20),
             legend.position = "none") +
       labs(title = paste0(this_pic, "\n"))
@@ -44,169 +93,103 @@ for(swatch in levels(d_tidy$swatch)) {
 
 # look at correlations of means (odd)
 d_swatches = d_tidy %>%
-  select(condition, worker_id, swatch, responseCoded) %>%
+  select(country, condition, worker_id, swatch, responseCoded) %>%
 #   spread(condition, responseCoded) %>%
-  group_by(swatch, condition) %>%
+  group_by(country, swatch, condition) %>%
   summarise(mean = mean(responseCoded)) %>%
   spread(condition, mean)
 
 View(d_swatches)
 
-d_swatches %>% select (-swatch) %>% cor()
+cor_india = d_swatches %>% 
+  filter(country == "india") %>%
+  select (-swatch, -country) %>% 
+  cor()
+cor_india
+
+cor_us = d_swatches %>% 
+  filter(country == "us") %>%
+  select (-swatch, -country) %>% 
+  cor()
+cor_us
 
 # score pictures for selection
 
 # look at relationship between mean and sd by picture
-ggplot(swatch_summary, aes(x = mean, y = sd)) +
+swatch_summary %>% 
+  ggplot(aes(x = mean, y = sd, colour = condition)) +
+  facet_grid(country ~ condition) +
   geom_jitter() +
   geom_smooth(method = loess) +
   theme_bw() +
   theme(text = element_text(size = 20),
         legend.position = "none") +
-  labs(title = "Animal ratings by picture:\nmean vs. standard deviation\n")
+  labs(title = "Mean vs. standard deviation for individual swatches,\nby country and condition\n")
 
-# look at histograms for means and sds
-# ... means
-ggplot(filter(swatch_summary, condition == "animal"), aes(x = mean)) +
-  geom_histogram() +
-  coord_cartesian(xlim = c(-3:3)) +
-  theme_bw() +
-  theme(text = element_text(size = 20),
-        legend.position = "none") +
-  labs(title = "Histogram of mean animal\nratings across pictures\n")
+# # look at histograms for means and sds
+# # ... means
+# ggplot(filter(swatch_summary, condition == "animal"), aes(x = mean)) +
+#   geom_histogram() +
+#   coord_cartesian(xlim = c(-3:3)) +
+#   theme_bw() +
+#   theme(text = element_text(size = 20),
+#         legend.position = "none") +
+#   labs(title = "Histogram of mean animal\nratings across pictures\n")
+# 
+# # ... sds
+# ggplot(filter(swatch_summary, condition == "animal"), aes(x = sd)) +
+#   geom_histogram() +
+#   coord_cartesian() +
+#   theme_bw() +
+#   theme(text = element_text(size = 20),
+#         legend.position = "none") +
+#   labs(title = "Histogram of sds of animal ratings\nacross pictures\n")
 
-# ... sds
-ggplot(filter(swatch_summary, condition == "animal"), aes(x = sd)) +
-  geom_histogram() +
-  coord_cartesian() +
-  theme_bw() +
-  theme(text = element_text(size = 20),
-        legend.position = "none") +
-  labs(title = "Histogram of sds of animal ratings\nacross pictures\n")
+# add animal ratings to swatch_summary
+animal_ratings = swatch_summary %>%
+  select(-sd, -n) %>%
+  spread(condition, mean) %>%
+  arrange(animal) %>%
+  select(country, swatch, animal) %>%
+  full_join(swatch_summary)
+View(animal_ratings)
 
-# look at mean animal ratings, sorted
-animal = swatch_summary_wide %>%
-  ggplot(aes(x = reorder(swatch, animal), y = animal, fill = "a")) +
-#   facet_wrap(~ condition) +
+# look at all ratings sorted by animal ratings
+ratings_us = animal_ratings %>% filter(country == "us") %>%
+  ggplot(aes(x = reorder(swatch, animal), y = mean, fill = condition)) +
+  facet_wrap(~ condition, ncol = 2) +
   geom_bar(stat = "identity", position = "identity", width = 0.5) +
-#   geom_errorbar(aes(ymin = mean - sd/sqrt(n),
-#                     ymax = mean  + sd/sqrt(n),
+#   geom_errorbar(aes(ymin = mean - 2*sd/sqrt(n),
+#                     ymax = mean + 2*sd/sqrt(n),
 #                     width = 0.1)) +
   coord_cartesian(ylim = c(-3, 3)) +
   theme_bw() +
   theme(text = element_text(size = 20),
         legend.position = "none",
-        axis.text.x = element_text(angle = 60,
-                                   hjust = 1)) +
-  scale_fill_brewer(palette = "Set1") +
-  labs(title = "Mean ANIMAL ratings by picture (sorted)\n",
+        axis.text.x = element_blank()) +
+#         axis.text.x = element_text(angle = 60,
+#                                    hjust = 1)) +
+  scale_fill_brewer(palette = "Set2") +
+  labs(title = "Mean ratings by picture (sorted): US\n",
        x = "Pictures (sorted by animal rating)")
-animal
+ratings_us
 
-think = swatch_summary_wide %>%
-  ggplot(aes(x = reorder(swatch, animal), y = think, fill = "a")) +
-  #   facet_wrap(~ condition) +
+# look at all ratings sorted by animal ratings
+ratings_ind = animal_ratings %>% filter(country == "india") %>%
+  ggplot(aes(x = reorder(swatch, animal), y = mean, fill = condition)) +
+  facet_wrap(~ condition, ncol = 2) +
   geom_bar(stat = "identity", position = "identity", width = 0.5) +
-  #   geom_errorbar(aes(ymin = mean - sd/sqrt(n),
-  #                     ymax = mean  + sd/sqrt(n),
+  #   geom_errorbar(aes(ymin = mean - 2*sd/sqrt(n),
+  #                     ymax = mean + 2*sd/sqrt(n),
   #                     width = 0.1)) +
   coord_cartesian(ylim = c(-3, 3)) +
   theme_bw() +
   theme(text = element_text(size = 20),
         legend.position = "none",
-        axis.text.x = element_text(angle = 60,
-                                   hjust = 1)) +
-  scale_fill_brewer(palette = "Set1") +
-  labs(title = "Mean THINK ratings by picture (sorted)\n",
+        axis.text.x = element_blank()) +
+  #         axis.text.x = element_text(angle = 60,
+  #                                    hjust = 1)) +
+  scale_fill_brewer(palette = "Set2") +
+  labs(title = "Mean ratings by picture (sorted): INDIA\n",
        x = "Pictures (sorted by animal rating)")
-think
-
-happy = swatch_summary_wide %>%
-  ggplot(aes(x = reorder(swatch, animal), y = happy, fill = "a")) +
-  #   facet_wrap(~ condition) +
-  geom_bar(stat = "identity", position = "identity", width = 0.5) +
-  #   geom_errorbar(aes(ymin = mean - sd/sqrt(n),
-  #                     ymax = mean  + sd/sqrt(n),
-  #                     width = 0.1)) +
-  coord_cartesian(ylim = c(-3, 3)) +
-  theme_bw() +
-  theme(text = element_text(size = 20),
-        legend.position = "none",
-        axis.text.x = element_text(angle = 60,
-                                   hjust = 1)) +
-  scale_fill_brewer(palette = "Set1") +
-  labs(title = "Mean HAPPY ratings by picture (sorted)\n",
-       x = "Pictures (sorted by animal rating)")
-happy
-
-feelings = swatch_summary_wide %>%
-  ggplot(aes(x = reorder(swatch, animal), y = feelings, fill = "a")) +
-  #   facet_wrap(~ condition) +
-  geom_bar(stat = "identity", position = "identity", width = 0.5) +
-  #   geom_errorbar(aes(ymin = mean - sd/sqrt(n),
-  #                     ymax = mean  + sd/sqrt(n),
-  #                     width = 0.1)) +
-  coord_cartesian(ylim = c(-3, 3)) +
-  theme_bw() +
-  theme(text = element_text(size = 20),
-        legend.position = "none",
-        axis.text.x = element_text(angle = 60,
-                                   hjust = 1)) +
-  scale_fill_brewer(palette = "Set1") +
-  labs(title = "Mean FEELINGS ratings by picture (sorted)\n",
-       x = "Pictures (sorted by animal rating)")
-feelings
-
-sense = swatch_summary_wide %>%
-  ggplot(aes(x = reorder(swatch, animal), y = sense, fill = "a")) +
-  #   facet_wrap(~ condition) +
-  geom_bar(stat = "identity", position = "identity", width = 0.5) +
-  #   geom_errorbar(aes(ymin = mean - sd/sqrt(n),
-  #                     ymax = mean  + sd/sqrt(n),
-  #                     width = 0.1)) +
-  coord_cartesian(ylim = c(-3, 3)) +
-  theme_bw() +
-  theme(text = element_text(size = 20),
-        legend.position = "none",
-        axis.text.x = element_text(angle = 60,
-                                   hjust = 1)) +
-  scale_fill_brewer(palette = "Set1") +
-  labs(title = "Mean SENSE ratings by picture (sorted)\n",
-       x = "Pictures (sorted by animal rating)")
-sense
-
-hungry = swatch_summary_wide %>%
-  ggplot(aes(x = reorder(swatch, animal), y = hungry, fill = "a")) +
-  #   facet_wrap(~ condition) +
-  geom_bar(stat = "identity", position = "identity", width = 0.5) +
-  #   geom_errorbar(aes(ymin = mean - sd/sqrt(n),
-  #                     ymax = mean  + sd/sqrt(n),
-  #                     width = 0.1)) +
-  coord_cartesian(ylim = c(-3, 3)) +
-  theme_bw() +
-  theme(text = element_text(size = 20),
-        legend.position = "none",
-        axis.text.x = element_text(angle = 60,
-                                   hjust = 1)) +
-  scale_fill_brewer(palette = "Set1") +
-  labs(title = "Mean HUNGRY ratings by picture (sorted)\n",
-       x = "Pictures (sorted by animal rating)")
-hungry
-
-pain = swatch_summary_wide %>%
-  ggplot(aes(x = reorder(swatch, animal), y = pain, fill = "a")) +
-  #   facet_wrap(~ condition) +
-  geom_bar(stat = "identity", position = "identity", width = 0.5) +
-  #   geom_errorbar(aes(ymin = mean - sd/sqrt(n),
-  #                     ymax = mean  + sd/sqrt(n),
-  #                     width = 0.1)) +
-  coord_cartesian(ylim = c(-3, 3)) +
-  theme_bw() +
-  theme(text = element_text(size = 20),
-        legend.position = "none",
-        axis.text.x = element_text(angle = 60,
-                                   hjust = 1)) +
-  scale_fill_brewer(palette = "Set1") +
-  labs(title = "Mean PAIN ratings by picture (sorted)\n",
-       x = "Pictures (sorted by animal rating)")
-pain
+ratings_ind
