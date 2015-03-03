8f8995257214d8534908e67df3a53055b507e42e
kgweisman <kweisman@stanford.edu>
2015-03-02 22:48:24 -0800

    make animal, think, feelings, etc. questions all between subjects

--- experiment/conditions.js
+++ experiment/conditions.js
@@ -1,11 +1,32 @@
 // define conditions
 var conditions = [
 	{
-		condition: "animalCategorization",
-		questions: ["is an animal"]
+		condition: "animal",
+		question: "is an animal"
 	},
 	{
-		condition: "propertyInference",
-		questions: ["can think", "has feelings", "can sense things nearby", "can feel happy", "can feel hungry", "can feel pain"]
+		condition: "think",
+		question: "can think"
+	},
+	{
+		condition: "feelings",
+		question: "has feelings"
+	},
+	{
+		condition: "sense",
+		question: "can sense things nearby"
+	},
+	{
+		condition: "happy",
+		question: "can feel happy"
+	},
+	{
+		condition: "hungry",
+		question: "can feel hungry"
+	},
+	{
+		condition: "pain",
+		question: "can feel pain"
 	}
 ];
+
