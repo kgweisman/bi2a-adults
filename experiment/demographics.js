781d3bdc7509dde120260aa0be1314cd86be9a68
kgweisman <kweisman@stanford.edu>
2015-03-02 23:55:17 -0800

    try to fix submission problem

--- experiment/demographics.js
+++ experiment/demographics.js
@@ -1,31 +1,28 @@
 /* set up demographics slide */
 
-// set up button behavior
-$('.slide#demographics button').click(function() { 
+var clickHandler = function(event) {
+	console.log("click handler");
 
-	console.log('click')
 	// record demographic info...
-	// text inputs
 	experiment.allData.age = $('input#age', '#demographicsForm').val();
-
-	// text areas
-	experiment.allData.comments = $('.slide#demographics textarea#comments').val();
-
-	// multiple choice radios
 	experiment.allData.gender = $('input[name=gender]:checked', '#demographicsForm').val();
 	experiment.allData.education = $('input[name=education]:checked', '#demographicsForm').val();
-	experiment.allData.englishNative = $('input[name=englishNative]:checked', '#demographicsForm').val();
-
-	// multiple answer checkboxes
-	$('input[name=ethnicity]:checked', '#demographicsForm').each(function() {
-		experiment.allData.ethnicity.push($(this).val());
-	});
 	$('input[name=religion]:checked', '#demographicsForm').each(function() {
 		experiment.allData.religion.push($(this).val());
 	});
+	$('input[name=ethnicity]:checked', '#demographicsForm').each(function() {
+		experiment.allData.ethnicity.push($(this).val());
+	});
+	experiment.allData.englishNative = $('input[name=englishNative]:checked', '#demographicsForm').val();
+	experiment.allData.comments = $('.slide#demographics textarea#comments').val();
+};
 
-	// end session
-	opener.turk.submit(experiment);
-	window.scrollTo(0, 0);
+// set up button behavior
+$('.slide#demographics button').click(function() { 
+	clickHandler();
+	// turk.submit(experiment);
+	// opener.turk.submit(experiment);
+	// window.scrollTo(0, 0);
 	showSlide("finished");
+	console.log("button selector");
 });
