973b7a6eea26223aced31098c08d44840a1eabb2
kgweisman <kweisman@stanford.edu>
2015-03-02 22:47:56 -0800

    save version where inferences re properties are within subjects

---
+++ experiment/conditions_alt.js
@@ -0,0 +1,12 @@
+// define conditions
+var conditions = [
+	{
+		condition: "animalCategorization",
+		questions: ["is an animal"]
+	},
+	{
+		condition: "propertyInference",
+		questions: ["can think", "has feelings", "can sense things nearby", "can feel happy", "can feel hungry", "can feel pain"]
+	}
+];
+
