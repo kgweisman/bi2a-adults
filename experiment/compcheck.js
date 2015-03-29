3bbfdb4a1dca5347e54eb175b5275c94e57bf6ba
kgweisman <kweisman@stanford.edu>
2015-03-28 18:00:04 -0700

    require restatement in own words

--- experiment/compcheck.js
+++ experiment/compcheck.js
@@ -1,15 +1,27 @@
 /* set up comprehension check on instructions slide */
 	
+// display question
+$('.slide#instructions span#question-text').text(chosenCondition.question);
+
 // set up button behavior
 $('.slide#instructions button').click(function() {
-	
-	// record data
-	experiment.allData.comprehensionCheck = $('textarea#comprehensionCheck').val();
 
-	// set parameters of this session
-	experiment.trials = swatches.slice();
-	experiment.condition = chosenCondition.condition.slice();
+	if ($('textarea#comprehensionCheck').val() == "") {
+	window.alert("Please restate the sentence before continuing.");
+	} else if ($('textarea#comprehensionCheck').val() == "I think that this one "+chosenCondition.question) {
+	window.alert("Please restate the sentence in your own words.");
+	} else if ($('textarea#comprehensionCheck').val() == "I think that this one "+chosenCondition.question+".") {
+	window.alert("Please restate the sentence in your own words.");
+	} else {
+		// record data
+		experiment.allData.comprehensionCheck = $('textarea#comprehensionCheck').val();
 
-	// go to first trial
-	experiment.next();
+		// set parameters of this session
+		experiment.trials = swatches.slice();
+		experiment.condition = chosenCondition.condition.slice();
+
+		// go to first trial
+		experiment.next();
+	}
+
 });
