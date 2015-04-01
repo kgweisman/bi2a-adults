382154d245016d5c064ed9c927c7eb61c7247605
kgweisman <kweisman@stanford.edu>
2015-04-01 16:04:35 -0700

    limit to animal condition only

--- experiment/conditions.js
+++ experiment/conditions.js
@@ -3,30 +3,31 @@ var conditions = [
 	{
 		condition: "animal",
 		question: "is an animal"
-	},
-	{
-		condition: "think",
-		question: "can think"
-	},
-	{
-		condition: "feelings",
-		question: "has feelings"
-	},
-	{
-		condition: "sense",
-		question: "can sense things that are close by"
-	},
-	{
-		condition: "happy",
-		question: "can feel happy"
-	},
-	{
-		condition: "hungry",
-		question: "can feel hungry"
-	},
-	{
-		condition: "pain",
-		question: "can feel pain"
 	}
+	// ,
+	// {
+	// 	condition: "think",
+	// 	question: "can think"
+	// },
+	// {
+	// 	condition: "feelings",
+	// 	question: "has feelings"
+	// },
+	// {
+	// 	condition: "sense",
+	// 	question: "can sense things that are close by"
+	// },
+	// {
+	// 	condition: "happy",
+	// 	question: "can feel happy"
+	// },
+	// {
+	// 	condition: "hungry",
+	// 	question: "can feel hungry"
+	// },
+	// {
+	// 	condition: "pain",
+	// 	question: "can feel pain"
+	// }
 ];
 
