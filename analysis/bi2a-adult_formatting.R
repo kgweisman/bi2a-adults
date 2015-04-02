3ab5000bc91ca398d8492a04932baee19b4098ee
kgweisman <kweisman@stanford.edu>
2015-04-02 10:55:13 -0700

    fix problems reading json files

--- analysis/bi2a-adult_formatting.R
+++ analysis/bi2a-adult_formatting.R
@@ -20,15 +20,6 @@ setwd("/Users//kweisman/Documents/Research (Stanford)/Projects/BI2A/bi2a-adults/
 
 # mike's json for-loop
 files <- dir("production-results/")
-
-# hackily exclude problematic files for india (if including any fingerprint data):
-# files2 <- files[-9]
-# files3 <- files2[-24]
-
-# hackily exclude two problematic files (no idea why)
-files <- files[-96] #3OLQQLKKNTYHBUCSBD20JHUSU0PJEU.json
-files <- files[-126] #3X73LLYYQ2NPEUUFGC2YXR8MVZCHN0.json
-
 d.raw <- data.frame()
 for (f in files) {
   # gather files
@@ -36,7 +27,7 @@ for (f in files) {
   
   # parse JSON object
   jd <- fromJSON(paste(readLines(jf), collapse=""))
-
+  
   # store relevant variables in dataframe 
   id <- data.frame(
     # subject-level data: identity
@@ -44,14 +35,20 @@ for (f in files) {
     worker_id = jd$WorkerId,
     
     # subject-level data: demographics
-    education = jd$answers$data$allData$education,
-    english_native = jd$answers$data$allData$englishNative,
-    ethnicity = jd$answer$data$allData$ethnicity,
-    gender = jd$answers$data$allData$gender,
-    age = jd$answers$data$allData$age,
-    religion = jd$answers$data$allData$religion,
+    education = ifelse(is.null(jd$answer$data$allData$education) == TRUE, "NA",
+                    jd$answer$data$allData$education),
+    english_native = ifelse(is.null(jd$answer$data$allData$english_native) == TRUE, "NA",
+                       jd$answer$data$allData$english_native),
+    ethnicity = ifelse(is.list(jd$answer$data$allData$ethnicity) == TRUE, "NA",
+                       jd$answer$data$allData$ethnicity),
+    gender = ifelse(is.null(jd$answer$data$allData$gender) == TRUE, "NA",
+                       jd$answer$data$allData$gender),
+    age = ifelse(is.null(jd$answer$data$allData$age) == TRUE, "NA",
+                    jd$answer$data$allData$age),
+    religion = ifelse(is.list(jd$answer$data$allData$religion) == TRUE, "NA",
+                      jd$answer$data$allData$religion),
     comments = jd$answers$data$allData$comments,
-                   
+    
     # trial-level data:                    
     trialNum = jd$answers$data$allData$trialData$trialNum,
     swatch = jd$answers$data$allData$trialData$swatch,
@@ -62,11 +59,10 @@ for (f in files) {
   # bind into same dataframe
   d.raw <- bind_rows(d.raw, id)
 }
-
 glimpse(d.raw)
 
 # clean up variables
-d_tidy_01 = d.raw %>%
+d_tidy = d.raw %>%
   mutate(condition = factor(condition),
          worker_id = factor(worker_id),
          gender = factor(gender),
@@ -78,15 +74,15 @@ d_tidy_01 = d.raw %>%
          trialNum = as.integer(trialNum),
          response = factor(response))
 
-glimpse(d_tidy_01)
+glimpse(d_tidy)
 
 # fix miscoding of responseCoded
-d_tidy_01$responseCoded[d_tidy_01$response == "definitely no"] = -3
+d_tidy$responseCoded[d_tidy$response == "definitely no"] = -3
   
-glimpse(d_tidy_01)
+glimpse(d_tidy)
 
 # add country variable for us
-d_india_01 = d_tidy_01 %>%
+d_india_01 = d_tidy %>%
   mutate(country = factor("india"))
 glimpse(d_india_01)
 
@@ -97,23 +93,6 @@ setwd("/Users//kweisman/Documents/Research (Stanford)/Projects/BI2A/bi2a-adults/
 
 # mike's json for-loop
 files <- dir("production-results/")
-
-# hackily exclude problematic files for us (if including any fingerprint data)
-# files2 <- files[-47]
-# files3 <- files2[-45]
-# files4 <- files3[-40]
-# files5 <- files4[-35]
-# files6 <- files5[-24]
-# files7 <- files6[-21]
-# files8 <- files7[-19]
-# files9 <- files8[-8]
-# files10 <- files9[-2]
-
-# still hackily exclude two files (no idea why!!)
-files <- files[-4] #3180JW2OT5LHFBJZ0C9XFNOFH5NJ5M.json
-files <- files[-44] #3BWI6RSP7HIORZBQA4D3GRPJ3417EL.json
-files <- files[-69] #3HMVI3QICK18MIDFLP8OMKQMIU5Y1T.json
-
 d.raw <- data.frame()
 for (f in files) {
   # gather files
@@ -129,12 +108,18 @@ for (f in files) {
     worker_id = jd$WorkerId,
     
     # subject-level data: demographics
-    education = jd$answers$data$allData$education,
-    english_native = jd$answers$data$allData$englishNative,
-    ethnicity = jd$answer$data$allData$ethnicity,
-    gender = jd$answers$data$allData$gender,
-    age = jd$answers$data$allData$age,
-    religion = jd$answers$data$allData$religion,
+    education = ifelse(is.null(jd$answer$data$allData$education) == TRUE, "NA",
+                       jd$answer$data$allData$education),
+    english_native = ifelse(is.null(jd$answer$data$allData$english_native) == TRUE, "NA",
+                            jd$answer$data$allData$english_native),
+    ethnicity = ifelse(is.list(jd$answer$data$allData$ethnicity) == TRUE, "NA",
+                       jd$answer$data$allData$ethnicity),
+    gender = ifelse(is.null(jd$answer$data$allData$gender) == TRUE, "NA",
+                    jd$answer$data$allData$gender),
+    age = ifelse(is.null(jd$answer$data$allData$age) == TRUE, "NA",
+                 jd$answer$data$allData$age),
+    religion = ifelse(is.list(jd$answer$data$allData$religion) == TRUE, "NA",
+                      jd$answer$data$allData$religion),
     comments = jd$answers$data$allData$comments,
     
     # trial-level data:                    
@@ -147,11 +132,10 @@ for (f in files) {
   # bind into same dataframe
   d.raw <- bind_rows(d.raw, id)
 }
-
 glimpse(d.raw)
 
 # clean up variables
-d_tidy_01 = d.raw %>%
+d_tidy = d.raw %>%
   mutate(condition = factor(condition),
          worker_id = factor(worker_id),
          gender = factor(gender),
@@ -163,15 +147,15 @@ d_tidy_01 = d.raw %>%
          trialNum = as.integer(trialNum),
          response = factor(response))
 
-glimpse(d_tidy_01)
+glimpse(d_tidy)
 
 # fix miscoding of responseCoded
-d_tidy_01$responseCoded[d_tidy_01$response == "definitely no"] = -3
+d_tidy$responseCoded[d_tidy$response == "definitely no"] = -3
 
-glimpse(d_tidy_01)
+glimpse(d_tidy)
 
 # add country variable for us
-d_us_01 = d_tidy_01 %>%
+d_us_01 = d_tidy %>%
   mutate(country = "us")
 
 # -------- COMBINE: RUN 1 -----------------------------------------------------
@@ -216,7 +200,6 @@ setwd("/Users//kweisman/Documents/Research (Stanford)/Projects/BI2A/bi2a-adults/
 
 # mike's json for-loop
 files <- dir("production-results/")
-
 d.raw <- data.frame()
 for (f in files) {
   # gather files
@@ -232,15 +215,18 @@ for (f in files) {
     worker_id = jd$WorkerId,
     
     # subject-level data: demographics
-    education = jd$answers$data$allData$education,
-    english_native = jd$answers$data$allData$englishNative,
-    ethnicity = jd$answer$data$allData$ethnicity,
-    gender = jd$answers$data$allData$gender,
-    age = jd$answers$data$allData$age,
-    religion = jd$answers$data$allData$religion,
-    
-    # subject-level data: free response
-    comp_check = jd$answers$data$allData$comprehensionCheck,
+    education = ifelse(is.null(jd$answer$data$allData$education) == TRUE, "NA",
+                       jd$answer$data$allData$education),
+    english_native = ifelse(is.null(jd$answer$data$allData$english_native) == TRUE, "NA",
+                            jd$answer$data$allData$english_native),
+    ethnicity = ifelse(is.list(jd$answer$data$allData$ethnicity) == TRUE, "NA",
+                       jd$answer$data$allData$ethnicity),
+    gender = ifelse(is.null(jd$answer$data$allData$gender) == TRUE, "NA",
+                    jd$answer$data$allData$gender),
+    age = ifelse(is.null(jd$answer$data$allData$age) == TRUE, "NA",
+                 jd$answer$data$allData$age),
+    religion = ifelse(is.list(jd$answer$data$allData$religion) == TRUE, "NA",
+                      jd$answer$data$allData$religion),
     comments = jd$answers$data$allData$comments,
     
     # trial-level data:                    
@@ -253,29 +239,13 @@ for (f in files) {
   # bind into same dataframe
   d.raw <- bind_rows(d.raw, id)
 }
-
 glimpse(d.raw)
 
-# clean up variables
-d_tidy_02A = d.raw %>%
-  mutate(condition = factor(condition),
-         worker_id = factor(worker_id),
-         gender = factor(gender),
-         ethnicity = factor(ethnicity),
-         religion = factor(religion),
-         age = as.numeric(age),
-         education = factor(education),
-         english_native = factor(english_native),
-         trialNum = as.integer(trialNum),
-         response = factor(response))
-
-glimpse(d_tidy_02A)
-
 # fix miscoding of responseCoded - NOT NEEDED FOR RUN 2
-# d_tidy_02A$responseCoded[d_tidy_02A$response == "definitely no"] = -3
+# d_tidy$responseCoded[d_tidy$response == "definitely no"] = -3
 
 # add country variable for us
-d_india_02A = d_tidy_02A %>%
+d_india_02A = d_tidy %>%
   mutate(country = factor("india"))
 
 # -------- INDIA: RUN 2B ------------------------------------------------------
@@ -285,7 +255,6 @@ setwd("/Users//kweisman/Documents/Research (Stanford)/Projects/BI2A/bi2a-adults/
 
 # mike's json for-loop
 files <- dir("production-results/")
-
 d.raw <- data.frame()
 for (f in files) {
   # gather files
@@ -301,15 +270,18 @@ for (f in files) {
     worker_id = jd$WorkerId,
     
     # subject-level data: demographics
-    education = jd$answers$data$allData$education,
-    english_native = jd$answers$data$allData$englishNative,
-    ethnicity = jd$answer$data$allData$ethnicity,
-    gender = jd$answers$data$allData$gender,
-    age = jd$answers$data$allData$age,
-    religion = jd$answers$data$allData$religion,
-    
-    # subject-level data: free response
-    comp_check = jd$answers$data$allData$comprehensionCheck,
+    education = ifelse(is.null(jd$answer$data$allData$education) == TRUE, "NA",
+                       jd$answer$data$allData$education),
+    english_native = ifelse(is.null(jd$answer$data$allData$english_native) == TRUE, "NA",
+                            jd$answer$data$allData$english_native),
+    ethnicity = ifelse(is.list(jd$answer$data$allData$ethnicity) == TRUE, "NA",
+                       jd$answer$data$allData$ethnicity),
+    gender = ifelse(is.null(jd$answer$data$allData$gender) == TRUE, "NA",
+                    jd$answer$data$allData$gender),
+    age = ifelse(is.null(jd$answer$data$allData$age) == TRUE, "NA",
+                 jd$answer$data$allData$age),
+    religion = ifelse(is.list(jd$answer$data$allData$religion) == TRUE, "NA",
+                      jd$answer$data$allData$religion),
     comments = jd$answers$data$allData$comments,
     
     # trial-level data:                    
@@ -322,11 +294,10 @@ for (f in files) {
   # bind into same dataframe
   d.raw <- bind_rows(d.raw, id)
 }
-
 glimpse(d.raw)
 
 # clean up variables
-d_tidy_02B = d.raw %>%
+d_tidy = d.raw %>%
   mutate(condition = factor(condition),
          worker_id = factor(worker_id),
          gender = factor(gender),
@@ -338,18 +309,91 @@ d_tidy_02B = d.raw %>%
          trialNum = as.integer(trialNum),
          response = factor(response))
 
-glimpse(d_tidy_02B)
+glimpse(d_tidy)
 
 # fix miscoding of responseCoded - NOT NEEDED FOR RUN 2
-# d_tidy_02B$responseCoded[d_tidy_02B$response == "definitely no"] = -3
+# d_tidy$responseCoded[d_tidy$response == "definitely no"] = -3
 
 # add country variable for us
-d_india_02B = d_tidy_02B %>%
+d_india_02B = d_tidy %>%
   mutate(country = "india")
+
+# -------- US: RUN 2 ----------------------------------------------------------
+
+# set working directory for us
+setwd("/Users//kweisman/Documents/Research (Stanford)/Projects/BI2A/bi2a-adults/turk/run-us_02/")
+
+# mike's json for-loop
+files <- dir("production-results/")
+d.raw <- data.frame()
+for (f in files) {
+  # gather files
+  jf <- paste("production-results/",f,sep="")
+  
+  # parse JSON object
+  jd <- fromJSON(paste(readLines(jf), collapse=""))
+  
+  # store relevant variables in dataframe 
+  id <- data.frame(
+    # subject-level data: identity
+    condition = jd$answers$data$allData$condition,
+    worker_id = jd$WorkerId,
+    
+    # subject-level data: demographics
+    education = ifelse(is.null(jd$answer$data$allData$education) == TRUE, "NA",
+                       jd$answer$data$allData$education),
+    english_native = ifelse(is.null(jd$answer$data$allData$english_native) == TRUE, "NA",
+                            jd$answer$data$allData$english_native),
+    ethnicity = ifelse(is.list(jd$answer$data$allData$ethnicity) == TRUE, "NA",
+                       jd$answer$data$allData$ethnicity),
+    gender = ifelse(is.null(jd$answer$data$allData$gender) == TRUE, "NA",
+                    jd$answer$data$allData$gender),
+    age = ifelse(is.null(jd$answer$data$allData$age) == TRUE, "NA",
+                 jd$answer$data$allData$age),
+    religion = ifelse(is.list(jd$answer$data$allData$religion) == TRUE, "NA",
+                      jd$answer$data$allData$religion),
+    comments = jd$answers$data$allData$comments,
+    
+    # trial-level data:                    
+    trialNum = jd$answers$data$allData$trialData$trialNum,
+    swatch = jd$answers$data$allData$trialData$swatch,
+    response = jd$answers$data$allData$trialData$response,
+    responseCoded = jd$answers$data$allData$trialData$responseCoded,
+    rt = jd$answers$data$allData$trialData$rt)
+  
+  # bind into same dataframe
+  d.raw <- bind_rows(d.raw, id)
+}
+glimpse(d.raw)
+
+# clean up variables
+d_tidy = d.raw %>%
+  mutate(condition = factor(condition),
+         worker_id = factor(worker_id),
+         gender = factor(gender),
+         ethnicity = factor(ethnicity),
+         religion = factor(religion),
+         age = as.numeric(age),
+         education = factor(education),
+         english_native = factor(english_native),
+         trialNum = as.integer(trialNum),
+         response = factor(response))
+
+glimpse(d_tidy)
+
+# # fix miscoding of responseCoded - NOT NEEDED FOR RUN 2
+# d_tidy$responseCoded[d_tidy$response == "definitely no"] = -3
+
+glimpse(d_tidy)
+
+# add country variable for us
+d_us_02 = d_tidy %>%
+  mutate(country = "us")
 
 # -------- COMBINE: RUN 2 -----------------------------------------------------
 
 d_tidy_02 = full_join(d_india_02A, d_india_02B) %>%
+  full_join(d_us_02) %>%
   mutate(worker_id = factor(worker_id),
          education = factor(education, 
                             levels = c("hs_diploma", 
@@ -379,4 +423,3 @@ glimpse(d_tidy_02)
 
 # write to csv
 write.csv(d_tidy_02, "/Users/kweisman/Documents/Research (Stanford)/Projects/BI2A/bi2a-adults/data/run-india-and-us_02.csv")
-
