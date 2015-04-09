36f1bc68a2f72077aeef098f83c84c721835f714
kgweisman <kweisman@stanford.edu>
2015-04-08 20:49:37 -0700

    comment out all conditions except for think, emotions, sense, hungry, and pain

--- experiment/conditions.js
+++ experiment/conditions.js
@@ -1,33 +1,37 @@
 // define conditions
 var conditions = [
-	{
-		condition: "animal",
-		question: "is an animal"
-	}
-	// ,
 	// {
-	// 	condition: "think",
-	// 	question: "can think"
-	// },
+	// 	condition: "animal",
+	// 	question: "is an animal"
+	// }
+	// ,
+	{
+		condition: "think",
+		question: "can think"
+	},
+	{
+		condition: "emotions",
+		question: "can feel emotions"
+	},
 	// {
 	// 	condition: "feelings",
 	// 	question: "has feelings"
 	// },
-	// {
-	// 	condition: "sense",
-	// 	question: "can sense things that are close by"
-	// },
+	{
+		condition: "sense",
+		question: "can sense things that are close by"
+	},
 	// {
 	// 	condition: "happy",
 	// 	question: "can feel happy"
 	// },
-	// {
-	// 	condition: "hungry",
-	// 	question: "can feel hungry"
-	// },
-	// {
-	// 	condition: "pain",
-	// 	question: "can feel pain"
-	// }
+	{
+		condition: "hungry",
+		question: "can feel hungry"
+	},
+	{
+		condition: "pain",
+		question: "can feel pain"
+	}
 ];
 
